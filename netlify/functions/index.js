568f5f72a00bc1997e2203bd68809fdfd4469e13
ortiqovn2fx-lab <ortiqovn2fx@gmail.com>
2026-04-24 01:20:37 +0000

    Complete cleanup: move to index.js and fix bundling config

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -1,10 +1,9 @@
 const serverless = require("serverless-http");
 const express = require("express");
-
 const app = express();
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from API!" });
+  res.json({ message: "API is working!" });
 });
 
 module.exports.handler = serverless(app);
