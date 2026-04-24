cac9663060417ef07ed0bf75ec0aef5a1febe630
ortiqovn2fx-lab <ortiqovn2fx@gmail.com>
2026-04-24 01:00:52 +0000

    Final attempt: pure CommonJS and external modules

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "API is working!" });
+  res.json({ message: "Hello from API!" });
 });
 
-exports.handler = serverless(app);
+module.exports.handler = serverless(app);
